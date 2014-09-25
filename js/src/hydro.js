7f9ec500348f53edd02a5c84f8c65f5cb7996c3f
tmaret <tmaret@adobe.com>
2014-09-25 20:47:05 +0200

    HYDRO-6 - Add support for cylinder solids
    * Add Solid class
    * Add Composite solid class
    * Add unit tests to cover the cylinder solids and composite solids

--- js/src/hydro.js
+++ js/src/hydro.js
@@ -13,146 +13,206 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 org = {};
-
 org.wildbits = {};
 
 org.wildbits.hydro = (function () {
 
     'use strict';
 
-    return {
+    /**
+     * Return the buoyancy of a solid, fully immersed in a liquid with the given density.
+     *
+     * @param {number} density The density of the liquid containing the solid in {@code kg•m^-3}
+     * @return {number} The resulting buoyancy in {@code kg} or {@code null} if undefined.
+     *                  A positive resulting buoyancy indicates the solid is positively buoyant.
+     */
+    var buoyancy = function(density) {
+        return density * this.volume() - this.mass();
+    };
+
+    /**
+     * Return the density of a solid.
+     *
+     * @return {number} density The density of the solid in {@code kg•m^-3}
+     */
+    var density = function() {
+        return this.mass() / this.volume();
+    };
+
+    /**
+     * Represents a solid cylinder filled with a gaz under pressure.
+     *
+     * @param {number} capacity The capacity of the cylinder in {@code m^3}
+     * @param {number} density The density of the cylinder material in {@code kg•m^-3}
+     * @param {number} mass1 The mass of the cylinder material in {@code kg}
+     * @param {number} mass2 The mass of the gaz contained in the cylinder in {@code kg}
+     */
+    var Cylinder = function(capacity, density, mass1, mass2) {
 
         /**
-         * Represent a composite solid.
-         *
-         * @param volume in {@code m^-3}
-         * @param mass in {@code kg}
+         * @return {number} The external volume of the cylinder in {@code m^3}
          */
-        Solid: function(volume, mass) {
-
-            return {
-
-                /**
-                 * @return {number} the volume of the solid in m^-3
-                 */
-                volume: function() {
-                    return volume;
-                },
-
-                /**
-                 * @return {number} the mass of the solid in kg
-                 */
-                mass: function() {
-                    return mass;
-                },
-
-                /**
-                 * @return {number} density of the solid in kg•m^-3
-                 */
-                density: function() {
-                    return this.mass() / this.volume();
-                },
-
-                /**
-                 * Return the buoyancy of the solid fully immersed in a liquid with the given density.
-                 *
-                 * @param density {number} the density of the liquid containing the solid in {@code kg•m^-3}
-                 * @return {number} the resulting buoyancy in {@code kg} or {@code null} if undefined. A positive resulting buoyancy indicates the solid is positively buoyant.
-                 */
-                buoyancy: function(density) {
-                    return density * this.volume() - this.mass();
-                },
-
-                /**
-                 * Extend the solid with another solid.
-                 *
-                 * @param solid {Solid} the solid extending the current solid
-                 * @return {Solid} this
-                 */
-                extend: function(solid) {
-                    mass += solid.mass();
-                    volume += solid.volume();
-                    return this;
-                }
-
-            };
-        },
+        this.volume = function() {
+            // volume of material = mass / density
+            // internal volume = capacity
+            // external volume = volume of material + internal volume
+            return (mass1 / density) + capacity;
+        };
 
         /**
-         * Salt Water Density model as defined in paper:
+         * @return {number} The mass of the cylinder and its gaz in {@code kg}
+         */
+        this.mass = function() {
+            return mass1 + mass2;
+        }
+
+    };
+
+    Cylinder.prototype.buoyancy = buoyancy;
+    Cylinder.prototype.density = density;
+
+    /**
+     * Represent an homogeneous solid.
+     *
+     * @param volume The solid volume in {@code m^-3}
+     * @param mass The solid mass in {@code kg}
+     */
+    var Solid = function(volume, mass) {
+
+        /**
+         * @return {number} The volume of the solid in {@code m^3}
+         */
+        this.volume = function () {
+            return volume;
+        };
+
+        /**
+         * @return {number} The mass of the solid in {@code kg}
+         */
+        this.mass = function () {
+            return mass;
+        };
+
+    };
+
+    Solid.prototype.buoyancy = buoyancy;
+    Solid.prototype.density = density;
+
+    /**
+     * Represent a composite of solids.
+     *
+     * @param {*} solids The list of solids that can quack 'volume()' and 'mass()'.
+     */
+    var Composite = function (solids) {
+
+        /**
+         * @returns {number} The resulting mass of the composite in {@code kg}
+         */
+        this.mass = function() {
+            var mass = 0;
+            for (var i = solids.length ; i-- ; ) {
+                mass += solids[i].mass();
+            }
+            return mass;
+        };
+
+        /**
+         * @returns {number} The resulting volume of the composite in {@code m^3}
+         */
+        this.volume = function () {
+            var volume = 0;
+            for (var i = solids.length ; i-- ; ) {
+                volume += solids[i].volume();
+            }
+            return volume;
+        };
+
+    };
+
+    Composite.prototype.buoyancy = buoyancy;
+    Composite.prototype.density = density;
+
+    /**
+     * Represent salt water.
+     */
+    var Seawater = function () {
+
+        var big = function (s) {
+            return parseFloat(s);
+        };
+
+        var validTemperature = function (temperature) {
+            return temperature >= MIN_TEMP && temperature < MAX_TEMP;
+        };
+
+        var validSalinity = function (salinity) {
+            return salinity >= MIN_SAL && salinity < MAX_SAL;
+        };
+
+        var A1 = big("999.9");
+        var A2 = big("0.02034");
+        var A3 = big("-0.006162");
+        var A4 = big("0.00002261");
+        var A5 = big("-0.00000004657");
+        var B1 = big("802.0");
+        var B2 = big("-2.001");
+        var B3 = big("0.01677");
+        var B4 = big("-0.00003060");
+        var B5 = big("-0.00001613");
+
+        var MIN_TEMP = 0.0;
+        var MAX_TEMP = 180.0;
+        var MIN_SAL = 0.0;
+        var MAX_SAL = 0.160;
+
+        /**
+         * Return the density of a substance at a given temperature.
+         *
+         * The density is computed based on the density model defined in the paper:
          * The thermophysical properties of seawater: A review of existing correlations and data, Eq. 8.
          * The paper is available on <a href="http://hdl.handle.net/1721.1/69157">MIT Open Access</a>.
+         *
+         * The model covers the following range for the input parameters:
+         *
+         * validTemp:     0 >= temperature < 180
+         * validSalinity: 0 >= salinity < 0.160
+         *
+         * The method return a negative number if it fails to compute the salinity.
+         *
+         * @param {number} temperature The temperature in {@code °C}
+         * @param {number} salinity The water salinity in {@code kg/kg}. Fresh water has salinity of {@code 0 kg/kg}
+         * @return {number} The density in {@code kg•m^-3} or {@code null} if undefined
          */
-        Seawater: function() {
-
-            var A1 = big("999.9");
-            var A2 = big("0.02034");
-            var A3 = big("-0.006162");
-            var A4 = big("0.00002261");
-            var A5 = big("-0.00000004657");
-            var B1 = big("802.0");
-            var B2 = big("-2.001");
-            var B3 = big("0.01677");
-            var B4 = big("-0.00003060");
-            var B5 = big("-0.00001613");
-
-            var MIN_TEMP = 0.0;
-            var MAX_TEMP = 180.0;
-            var MIN_SAL = 0.0;
-            var MAX_SAL = 0.160;
-
-            return {
-
-                /**
-                 * Return the density of a substance at a given temperature.
-                 * The model covers the following range for the input parameters:
-                 *
-                 * validTemp:     0 >= temperature < 180
-                 * validSalinity: 0 >= salinity < 0.160
-                 *
-                 * The method return a negative number if it fails to compute the salinity.
-                 *
-                 * @param temperature {number} the temperature in {@code °C}
-                 * @param salinity {number} the water salinity in {@code kg/kg}. Fresh water has salinity of {@code 0 kg/kg}
-                 * @return {number} the density in {@code kg•m^-3} or {@code null} if undefined
-                 */
-                density: function (temperature, salinity) {
-                    if (validSalinity(salinity) && validTemperature(temperature)) {
-                        var t2 = Math.pow(temperature,2);
-                        var t3 = Math.pow(temperature,3);
-                        var t4 = Math.pow(temperature,4);
-                        var s2 = Math.pow(salinity,2);
-                        return A1 +
-                            A2 * temperature +
-                            A3 * t2 +
-                            A4 * t3 +
-                            A5 * t4 +
-                            B1 * salinity +
-                            B2 * salinity * temperature +
-                            B3 * salinity * t2 +
-                            B4 * salinity * t3 +
-                            B5 * s2 * t2;
-                    } else {
-                        return -1;
-                    }
-                }
-            };
-
-            function big(s) {
-                return parseFloat(s);
+        this.density = function (temperature, salinity) {
+            if (validSalinity(salinity) && validTemperature(temperature)) {
+                var t2 = Math.pow(temperature,2);
+                var t3 = Math.pow(temperature,3);
+                var t4 = Math.pow(temperature,4);
+                var s2 = Math.pow(salinity,2);
+                return A1 +
+                    A2 * temperature +
+                    A3 * t2 +
+                    A4 * t3 +
+                    A5 * t4 +
+                    B1 * salinity +
+                    B2 * salinity * temperature +
+                    B3 * salinity * t2 +
+                    B4 * salinity * t3 +
+                    B5 * s2 * t2;
+            } else {
+                return -1;
             }
-
-            function validSalinity(salinity) {
-                return salinity >= MIN_SAL && salinity < MAX_SAL;
-            }
-
-            function validTemperature(temperature) {
-                return temperature >= MIN_TEMP && temperature < MAX_TEMP;
-            }
-
         }
+
+    };
+
+    return {
+
+        Cylinder  : Cylinder,
+        Solid     : Solid,
+        Composite : Composite,
+        Seawater  : Seawater
 
     };
 
--- js/test/test.js
+++ js/test/test.js
@@ -18,52 +18,54 @@ var hydro = require("../src/hydro.js");
 var assert = require("assert");
 
 var Solid = org.wildbits.hydro.Solid;
+var Composite = org.wildbits.hydro.Composite;
+var Cylinder = org.wildbits.hydro.Cylinder;
 var Seawater = org.wildbits.hydro.Seawater;
 
 var DELTA = 0.001;
 
 describe('Seawater', function(){
 
-    var seawater = Seawater();
+    var seawater = new Seawater();
 
     describe('#density()', function() {
         it('should return -1 when either of the parameters is out of range', function(){
-            assert.equal(-1, seawater.density(-1, 0.05));
-            assert.equal(-1, seawater.density(182, 0.05));
-            assert.equal(-1, seawater.density(30, -0.1));
-            assert.equal(-1, seawater.density(30, 0.2));
+            assertEqualsWithDelta(-1, seawater.density(-1, 0.05));
+            assertEqualsWithDelta(-1, seawater.density(182, 0.05));
+            assertEqualsWithDelta(-1, seawater.density(30, -0.1));
+            assertEqualsWithDelta(-1, seawater.density(30, 0.2));
         });
     });
 
     describe('#density()', function() {
         it('should return the correct density', function(){
-            assert.equal(true, delta(999.9, seawater.density(0, 0)));
-            assert.equal(true, delta(999.5093443, seawater.density(10, 0)));
-            assert.equal(true, delta(998.0154288, seawater.density(20, 0)));
-            assert.equal(true, delta(995.5371483, seawater.density(30, 0)));
-            assert.equal(true, delta(992.1822208, seawater.density(40, 0)));
-            assert.equal(true, delta(988.0471875, seawater.density(50, 0)));
-            assert.equal(true, delta(983.2174128, seawater.density(60, 0)));
-            assert.equal(true, delta(977.7670843, seawater.density(70, 0)));
-            assert.equal(true, delta(971.7592128, seawater.density(80, 0)));
-            assert.equal(true, delta(965.2456323, seawater.density(90, 0)));
-            assert.equal(true, delta(958.267, seawater.density(100, 0)));
-            assert.equal(true, delta(950.8527963, seawater.density(110, 0)));
-            assert.equal(true, delta(943.0213248, seawater.density(120, 0)));
+            assertEqualsWithDelta(999.9, seawater.density(0, 0));
+            assertEqualsWithDelta(999.5093443, seawater.density(10, 0));
+            assertEqualsWithDelta(998.0154288, seawater.density(20, 0));
+            assertEqualsWithDelta(995.5371483, seawater.density(30, 0));
+            assertEqualsWithDelta(992.1822208, seawater.density(40, 0));
+            assertEqualsWithDelta(988.0471875, seawater.density(50, 0));
+            assertEqualsWithDelta(983.2174128, seawater.density(60, 0));
+            assertEqualsWithDelta(977.7670843, seawater.density(70, 0));
+            assertEqualsWithDelta(971.7592128, seawater.density(80, 0));
+            assertEqualsWithDelta(965.2456323, seawater.density(90, 0));
+            assertEqualsWithDelta(958.267, seawater.density(100, 0));
+            assertEqualsWithDelta(950.8527963, seawater.density(110, 0));
+            assertEqualsWithDelta(943.0213248, seawater.density(120, 0));
             //
-            assert.equal(true, delta(1064.06, seawater.density(0, 0.08)));
-            assert.equal(true, delta(1062.2, seawater.density(10, 0.08)));
-            assert.equal(true, delta(1059.4908435072, seawater.density(20, 0.08)));
-            assert.equal(true, delta(1056.0359993912, seawater.density(30, 0.08)));
-            assert.equal(true, delta(1051.9287436288, seawater.density(40, 0.08)));
-            assert.equal(true, delta(1047.25092942, seawater.density(50, 0.08)));
-            assert.equal(true, delta(1042.0732331648, seawater.density(60, 0.08)));
-            assert.equal(true, delta(1036.4551544632, seawater.density(70, 0.08)));
-            assert.equal(true, delta(1030.4450161152, seawater.density(80, 0.08)));
-            assert.equal(true, delta(1024.0799641208, seawater.density(90, 0.08)));
-            assert.equal(true, delta(1017.38596768, seawater.density(100, 0.08)));
-            assert.equal(true, delta(1010.3778191928, seawater.density(110, 0.08)));
-            assert.equal(true, delta(1003.0591342592, seawater.density(120, 0.08)));
+            assertEqualsWithDelta(1064.06, seawater.density(0, 0.08));
+            assertEqualsWithDelta(1062.2, seawater.density(10, 0.08));
+            assertEqualsWithDelta(1059.4908435072, seawater.density(20, 0.08));
+            assertEqualsWithDelta(1056.0359993912, seawater.density(30, 0.08));
+            assertEqualsWithDelta(1051.9287436288, seawater.density(40, 0.08));
+            assertEqualsWithDelta(1047.25092942, seawater.density(50, 0.08));
+            assertEqualsWithDelta(1042.0732331648, seawater.density(60, 0.08));
+            assertEqualsWithDelta(1036.4551544632, seawater.density(70, 0.08));
+            assertEqualsWithDelta(1030.4450161152, seawater.density(80, 0.08));
+            assertEqualsWithDelta(1024.0799641208, seawater.density(90, 0.08));
+            assertEqualsWithDelta(1017.38596768, seawater.density(100, 0.08));
+            assertEqualsWithDelta(1010.3778191928, seawater.density(110, 0.08));
+            assertEqualsWithDelta(1003.0591342592, seawater.density(120, 0.08));
         });
     });
 
@@ -73,7 +75,7 @@ describe('Solid', function(){
 
     var volume = 100.25;
     var mass = 200.5;
-    var solid = Solid(volume, mass);
+    var solid = new Solid(volume, mass);
 
     describe('#volume()', function() {
         it('should return the volume provided', function(){
@@ -95,30 +97,100 @@ describe('Solid', function(){
 
     describe('#buoyancy()', function() {
         it('should return the buoyancy', function(){
-            assert.equal(0.0, Solid(1, 1000).buoyancy(1000));
-            assert.equal(500, Solid(1, 500).buoyancy(1000));
-            assert.equal(-1000, Solid(1, 2000).buoyancy(1000));
+            assertEqualsWithDelta(0.0, new Solid(1, 1000).buoyancy(1000));
+            assertEqualsWithDelta(500, new Solid(1, 500).buoyancy(1000));
+            assertEqualsWithDelta(-1000, new Solid(1, 2000).buoyancy(1000));
         });
     });
 
-    describe('#extend()', function() {
-        it('should return an extended solid', function(){
-            var extended = Solid(1,2).extend(Solid(3,4));
-            assert.equal(4, extended.volume());
-            assert.equal(6, extended.mass());
+});
+
+describe('Cylinder', function() {
+
+    var capacity = 0.012;
+    var density = 7850;
+    var mass1 = 15;
+    var emptyMass = 0;
+    var fullMass = 3.38;
+
+    var emptyCylinder = new Cylinder(capacity, density, mass1, emptyMass);
+    var fullCylinder = new Cylinder(capacity, density, mass1, fullMass);
+
+    describe('#volume()', function() {
+        it('should return the volume provided', function(){
+            assertEqualsWithDelta(0.01391, emptyCylinder.volume());
+            assertEqualsWithDelta(0.01391, fullCylinder.volume());
         });
     });
+
+    describe('#mass()', function() {
+        it('should return the mass provided', function(){
+            assertEqualsWithDelta(15, emptyCylinder.mass());
+            assertEqualsWithDelta(18.38, fullCylinder.mass());
+        });
+    });
+
+    describe('#density()', function() {
+        it('should return the density', function(){
+            assertEqualsWithDelta(1078.2967, emptyCylinder.density());
+            assertEqualsWithDelta(1321.2728, fullCylinder.density());
+        });
+    });
+
+    describe('#buoyancy()', function() {
+        it('should return the buoyancy', function(){
+            assertEqualsWithDelta(-0.6718, emptyCylinder.buoyancy(1030));
+            assertEqualsWithDelta(-4.0518, fullCylinder.buoyancy(1030));
+        });
+    });
+
+});
+
+describe('Composite', function(){
+
+    var s1 = new Solid(1,2);
+    var s2 = new Solid(3,4);
+    var composite = new Composite([s1, s2]);
+
+    describe('#volume()', function() {
+        it('should return the volume provided', function(){
+            assertEqualsWithDelta(4, composite.volume());
+        });
+    });
+
+    describe('#mass()', function() {
+        it('should return the mass provided', function(){
+            assertEqualsWithDelta(6, composite.mass());
+        });
+    });
+
+    describe('#density()', function() {
+        it('should return the density', function(){
+            assertEqualsWithDelta(1.5, composite.density());
+        });
+    });
+
+    describe('#buoyancy()', function() {
+        it('should return the buoyancy', function(){
+            assertEqualsWithDelta(3994, composite.buoyancy(1000));
+        });
+    });
+
 });
 
 describe('Sample', function(){
     it('execution', function() {
-        var seawater = Seawater();
-        var solid = Solid(1.0,1000.0).extend(Solid(0.1,1500.0));
+        var seawater = new Seawater();
+        var solid = new Composite([new Solid(1.0,1000.0), new Solid(0.1,1500.0)]);
         var density = seawater.density(20.5, 0.08);
         console.log("density: " + solid.buoyancy(density));
     });
 });
 
-function delta(expected, value) {
+var assertEqualsWithDelta = function (expected, value) {
+    assert.equal(true, delta(expected, value), (expected + ' == ' + value));
+};
+
+var delta = function (expected, value) {
     return Math.abs(expected - value) < DELTA;
-}
+};
